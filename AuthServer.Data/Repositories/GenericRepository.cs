6a287b3a4a5a2c58f9e8fad79e6fa70db31d867f
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2024-10-26 16:47:07 +0300

    Configure Database Configurations and Connections

---
+++ AuthServer.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using AuthServer.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Data.Repositories
+{
+    public class GenericRepository<T>(DbContext context) : IGenericRepository<T> where T : class
+    {
+        private readonly DbContext _context = context;
+        private readonly DbSet<T> _dbSet = context.Set<T>();
+
+        public async Task AddAsync(T Entity)
+        {
+            await _dbSet.AddAsync(Entity);
+        }
+
+        public void Delete(T Entity)
+        {
+            _dbSet.Remove(Entity);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity = await _dbSet.FindAsync(id);
+
+            if(entity != null)
+            {
+                _context.Entry(entity).State = EntityState.Detached;
+            }
+            return entity;
+        }
+
+        public T Update(T Entity)
+        {
+            _context.Entry(Entity).State = EntityState.Modified;
+            return Entity;
+        }
+
+        public IQueryable<T> Where(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+    }
+}
