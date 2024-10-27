baf9b84dc2381b3c6fa9091993e71dc3e428be9c
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2024-10-27 22:54:31 +0300

    MiniAPI's Tokenoptions added - appsettingjson program.cs configured

---
+++ AppFirst.API/Controllers/StockController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AppFirst.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStock()
+        {
+            var userName = HttpContext.User.Identity.Name; //Pull userId data from accessToken in payload 
+
+            var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier); //Pull userName data from accessToken in payload - claims (TokenService class Configures claimsTypes)
+
+            //Pull the necessary datas on Database by using relation with userId and userName
+            //StockId - StockQuantity - Category  UserId - UserName
+
+            return Ok($"Stock Operations => UserName : {userName} - UserId : {userIdClaim.Value} "); 
+        }
+    }
+}
--- AppSecond.API/Program.cs
+++ AppSecond.API/Program.cs
@@ -1,8 +1,17 @@
+using SharedLib.Configurations;
+using SharedLib.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.Configure<CustomTokenOptions>(builder.Configuration.GetSection("TokenOptions"));
+
+var tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<CustomTokenOptions>();
+
+builder.Services.AddCustomTokenAuth(tokenOptions);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,7 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
