d9c2f00e51a1043acfcdc177bbbc9dace89b5109
AHMET YAVUZ IŞIK <ayavuzisik@ogr.trakya.edu.tr>
2024-10-27 22:55:28 +0300

    Extensions added. SignService moved in.

---
+++ SharedLib/Extensions/CustomExceptionHandler.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using SharedLib.Dtos;
+using SharedLib.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SharedLib.Extensions
+{
+    public static class CustomExceptionHandler
+    {
+
+        public static void UseCustomExcepiton(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+
+                config.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+
+                    context.Response.ContentType = "application/json";
+
+                    var errorFeatures = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (errorFeatures != null)
+                    {
+                        var ex = errorFeatures.Error;
+
+                        ErrorDto errorDto = null;
+
+                        if (ex is CustomException)
+                        {
+                            errorDto = new ErrorDto(ex.Message, true);
+                        }
+                        else
+                        {
+                            errorDto = new ErrorDto(ex.Message, false);
+                        }
+
+
+                        var response = Response<NoDataDto>.Fail(errorDto, 500);
+
+                        await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                    }
+
+                });
+            });
+        }
+
+    }
+}
---
+++ SharedLib/Extensions/CustomTokenAuth.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using SharedLib.Configurations;
+using SharedLib.Services;
+
+namespace SharedLib.Extensions
+{
+    public static class CustomTokenAuth
+    {
+        public static IServiceCollection AddCustomTokenAuth(this IServiceCollection services,CustomTokenOptions tokenOptions)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, configureOptions =>
+            {
+                configureOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidIssuer = tokenOptions.Issuer,
+                    ValidAudience = tokenOptions.Audience[0],
+                    IssuerSigningKey = SignService.GetSymmetricSecurityKey(tokenOptions.SecurityKey),
+
+                    ValidateIssuerSigningKey = true,
+                    ValidateAudience = true,
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+
+                    ClockSkew = TimeSpan.Zero //for localtime differences
+                };
+            });
+
+            return services;
+        }
+    }
+}
